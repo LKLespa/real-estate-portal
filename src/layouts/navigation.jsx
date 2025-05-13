a3e78ad90d49eec20aa1a4b32cf2808b04b86cfb
Mbah Lesky <mbahlesky2@gmail.com>
2025-05-13 09:15:34 +0100

    drawer

--- src/layouts/navigation.jsx
+++ src/layouts/navigation.jsx
@@ -1,12 +1,14 @@
-import { SearchIcon } from "@chakra-ui/icons";
+import { HamburgerIcon, SearchIcon } from "@chakra-ui/icons";
 import {
   Avatar,
   Badge,
   Box,
   Button,
   Center,
+  CloseButton,
   Container,
   Divider,
+  Drawer,
   Flex,
   HStack,
   Heading,
@@ -21,8 +23,16 @@ import {
   MenuGroup,
   MenuItem,
   MenuList,
+  Portal,
   Spinner,
   VStack,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  useDisclosure,
 } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import { Navigate, Outlet, useNavigate } from "react-router";
@@ -39,30 +49,33 @@ export default function NavigationWrapper() {
   const { userData, signOut, getUserData, loading } = useAuth();
   const navigate = useNavigate();
 
+  const { isOpen, onOpen, onClose } = useDisclosure();
+const btnRef = React.useRef();
+
   console.log('Auth', auth)
 
-  if(loading) {
+  if (loading) {
     return (
-    <Box height='100vh' width='100vw'>
-      <Center height='100%' width='100%'><Spinner /></Center>
-    </Box>
+      <Box height='100vh' width='100vw'>
+        <Center height='100%' width='100%'><Spinner /></Center>
+      </Box>
     )
   }
-  
+
   return (
     <Container maxW="100vw" bg="gray.100" h="100vh">
       <VStack spacing={2} height="100%">
         <Box bg="white" width="100%" px={3} py={4}>
           <HStack justify="space-between" align="center" spacing={2}>
             <Link to="/">
-            <Heading flexShrink={0} size={["md", "lg"]} color="teal.600" display={["none", null, "block"]}>
-              Real Estate Portal
-            </Heading>
+              <Heading flexShrink={0} size={["md", "lg"]} color="teal.600" display={["none", null, "block"]}>
+                Real Estate Portal
+              </Heading>
             </Link>
             <Link to="/">
-            <Heading onClick={() => navigate('/')} size={["md", "lg"]} color="teal.600" display={["block", null, "none"]}>
-              REP
-            </Heading>
+              <Heading onClick={() => navigate('/')} size={["md", "lg"]} color="teal.600" display={["block", null, "none"]}>
+                REP
+              </Heading>
             </Link>
             <InputGroup maxW="500px" flexShrink={1}>
               <InputLeftElement>
@@ -73,19 +86,47 @@ export default function NavigationWrapper() {
               <Input placeholder="Search" ml="auto" boxShadow="base" />
             </InputGroup>
             <HStack spacing={2}>
+              <Box display={["block", null, "none"]}>
+                <IconButton isRound ref={btnRef} onClick={onOpen}>
+                  <HamburgerIcon />
+                </IconButton>
+              </Box>
+
+              <Drawer
+                isOpen={isOpen}
+                placement="right"
+                onClose={onClose}
+                finalFocusRef={btnRef}
+              >
+                <DrawerOverlay />
+                <DrawerContent>
+                  <DrawerCloseButton />
+                  <DrawerHeader>Menu</DrawerHeader>
+
+                  <DrawerBody>
+                    <Sidebar />
+                  </DrawerBody>
+
+                  <DrawerFooter>
+                    <Button variant="outline" mr={3} onClick={onClose}>
+                      Cancel
+                    </Button>
+                  </DrawerFooter>
+                </DrawerContent>
+              </Drawer>
               <Box display={["none", "block"]}>
-              <IconButton isRound >
-                <BiHeart />
-              </IconButton>
+                <IconButton isRound >
+                  <BiHeart />
+                </IconButton>
               </Box>
               {auth.currentUser ? <IconButton variant="unstyled" onClick={() => navigate('/profile')}>
                 <Avatar src="" height={"40px"} width={"40px"} />
-              </IconButton> : <Button onClick={() => navigate('/register')}>Create Account</Button> }
+              </IconButton> : <Button onClick={() => navigate('/register')}>Create Account</Button>}
             </HStack>
           </HStack>
         </Box>
         <Outlet />
       </VStack>
     </Container>
-  ) 
+  )
 }
